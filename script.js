09b0179bdf4bf3ddfe0c3ca04a7da554883c4ca4
NicTrixx23-2 <nico.scholl@loewenrot.de>
2024-06-28 09:00:14 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,19 @@
+body, html {
+    height: 100%;
+    margin: 0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: #f0f0f0;
+}
+
+.game-container {
+    width: 400px;
+    height: 400px;
+}
+
+canvas {
+    border: 1px solid #000;
+    display: block;
+    margin: 0 auto;
+}
